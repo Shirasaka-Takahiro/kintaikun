a12037706ce627690627293c8db3d8763da0b5ee
Shirasaka-Takahiro <kenshirou913@gmail.com>
2020-01-21 03:34:49 +0000

    fixed views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,11 @@ class UsersController < ApplicationController
     @totaltime_worktime = @events.sum(:worktime)
     @totaltime_breaktime = @events.sum(:breaktime)
     @total_travel_expenses = @events.sum(:travel_expenses)
+    @totaltime_paperworktime_hour = @totaltime_paperworktime / 60
+    @totaltime_lessontime_hour = @totaltime_lessontime / 60.round(2)
+    @totaltime_training_hour = @totaltime_trainingtime / 60.round(2)
+    @totaltime_worktime_hour = @totaltime_worktime / 60.round(2)
+    @totaltime_breaktime_hour = @totaltime_breaktime / 60.round(2)
   end
 
   def index
