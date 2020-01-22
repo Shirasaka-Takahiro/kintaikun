0cecada51dda6d177521c7ace5ef397dbb0e7f7a
Shirasaka-Takahiro <kenshirou913@gmail.com>
2020-01-22 16:09:34 +0000

    fixed some files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,11 @@ class UsersController < ApplicationController
     @totaltime_worktime = @events.sum(:worktime)
     @totaltime_breaktime = @events.sum(:breaktime)
     @total_travel_expenses = @events.sum(:travel_expenses)
-    @totaltime_paperworktime_hour = @totaltime_paperworktime / 60
-    @totaltime_lessontime_hour = @totaltime_lessontime / 60.round(2)
-    @totaltime_training_hour = @totaltime_trainingtime / 60.round(2)
-    @totaltime_worktime_hour = @totaltime_worktime / 60.round(2)
-    @totaltime_breaktime_hour = @totaltime_breaktime / 60.round(2)
+    @totaltime_paperworktime_hour = @totaltime_paperworktime.to_f / 60
+    @totaltime_lessontime_hour = @totaltime_lessontime.to_f / 60
+    @totaltime_training_hour = @totaltime_trainingtime.to_f / 60
+    @totaltime_worktime_hour = @totaltime_worktime.to_f / 60
+    @totaltime_breaktime_hour = @totaltime_breaktime.to_f / 60
   end
 
   def index
