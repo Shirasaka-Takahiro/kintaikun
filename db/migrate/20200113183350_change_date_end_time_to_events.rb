5ffe2e6e6478f0491d5a64df223484d05334ad70
Shirasaka-Takahiro <kenshirou913@gmail.com>
2020-01-31 16:34:07 +0000

    changed migraiotn file

--- db/migrate/20200113183350_change_date_end_time_to_events.rb
+++ db/migrate/20200113183350_change_date_end_time_to_events.rb
@@ -1,5 +1,5 @@
 class ChangeDateEndTimeToEvents < ActiveRecord::Migration[6.0]
   def change
-    change_column :events, :breaktime, :integer
+    change_column :events, :breaktime, 'integer USING CAST(worktime AS integer)'
   end
 end
